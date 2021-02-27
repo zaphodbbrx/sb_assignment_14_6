a8c227df558098615477e3c345ae95bd9fb14fbe
zaphod_b <commondore.ne@gmail.com>
2021-02-27 22:23:58 +0300

    compare floats

---
+++ compare_float.h
@@ -0,0 +1,4 @@
+#ifndef ASSIGNMENT_14_6_COMPARE_FLOAT_H
+#define ASSIGNMENT_14_6_COMPARE_FLOAT_H
+void compareFloat(std::string s1, std::string s2);
+#endif //ASSIGNMENT_14_6_COMPARE_FLOAT_H
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "tictactoe.h"
 #include "email.h"
 #include "ip_address.h"
+#include "compare_float.h"
 
 void test1(){
     std::string test_s = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
@@ -26,14 +27,22 @@ void test3(){
 }
 
 void test4(){
-    std::string testStr = "192.168.100.1";
+    std::string testStr = "192.168.O10.1";
     std::cout << checkIP(testStr) << "\n";
+}
+
+void test5(){
+    std::string s1 = ".52";
+    std::string s2 = "0.52";
+
+    compareFloat(s1, s2);
 }
 
 int main() {
 //    test1();
 //    test2();
 //    test3();
-    test4();
+//    test4();
+    test5();
     return 0;
 }
